14aa58a7f434caa52b737560f92b8d7a742f4a1a
Jens A. Koch <jakoch@web.de>
2025-01-26 17:55:28 +0100

    update SPDX-FileCopyrightText year

--- .devcontainer/scripts/entrypoint.sh
+++ .devcontainer/scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# SPDX-FileCopyrightText: 2023,2024 Jens A. Koch
+# SPDX-FileCopyrightText: 2023-2025 Jens A. Koch
 # SPDX-License-Identifier: MIT
 # This file is part of https://github.com/jakoch/cpp-devbox
 
